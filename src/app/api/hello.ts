35ca0c611524826ea7a5a887e91c7d8ca030f23b
MatheusMA678 <matheusma2510@hotmail.com>
2023-08-01 20:00:22 -0400

    initial commit

---
+++ src/app/api/hello.ts
@@ -0,0 +1,11 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+
+export default function Hello(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { name } = req.query
+    return res.send({ hello: name })
+  }
+  catch (error) {
+    return res.status(400).send({ error })
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,84 @@
 import Image from 'next/image'
 
-export default function Home() {
+import FallbackPoster from '@/assets/movie-poster.png'
+import Logo from '@/assets/logo.svg'
+
+const token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2FlNDJkMmY1ODhlOWE0YzI0ZjM1OGU3ZGU0MzJmMSIsInN1YiI6IjY0MjkwZTg3NjA5NzUwMDA3NzBlNzQ0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LFhVr02zLRnpnG4k7fKu_iFypJ9E6myABVSFayeHCdI"
+
+interface IMovie {
+  id: number;
+  title: string;
+  release_date: string;
+  runtime: string;
+  vote_average: number;
+  poster_path: string;
+  overview: string;
+}
+
+async function getMovie(): Promise<IMovie> {
+  const res = await fetch("https://api.themoviedb.org/3/movie/298618?language=pt-BR", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  return res.json()
+}
+
+export default async function Home() {
+  // const movie = await getMovie()
+  let movie: any;
+  const moviePoster = movie ? "https://image.tmdb.org/t/p/original" + movie.poster_path : FallbackPoster
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="bg-zinc-800 text-zinc-100 px-16 py-12 min-h-screen flex flex-col">
+      <header className="flex items-center justify-between z-10">
+        <Image src={Logo} alt='Movie AI' />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <button className="rounded bg-gradient-to-r from-purple-700 to-pink-700 text-sm font-medium leading-none flex items-center justify-center py-2 px-4 shadow-lg">Nova recomendação</button>
+      </header>
+
+      <main className="flex-1 flex items-center">
+        {/* Movie Image Background */}
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={moviePoster}
+          className="h-screen w-[500px] absolute top-0 left-0 blur-[150px]"
+          alt="Movie Poster"
+          width={200}
+          height={300}
         />
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="flex gap-4 z-10 w-fit">
+          {/* Movie Poster */}
+          <Image
+            src={moviePoster}
+            className="h-[40%] bg-zinc-500 rounded-lg"
+            width={200}
+            height={300}
+            alt="Movie Poster"
+          />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/* Movie Info */}
+          <div className="flex flex-col justify-between">
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            {/* Movie Title & Desc */}
+            <div className="flex flex-col gap-2">
+              <div className="flex items-cetner gap-4">
+                <h1 className="text-xl font-semibold">{movie ? movie.title : "Movie name"}</h1>
+                <span className="font-semibold text-lg text-yellow-500">{movie ? movie.vote_average : "5.0"}</span>
+              </div>
+              <p className="text-xs text-zinc-300 ml-1 max-w-sm">{movie ? movie.overview : "Movie description"}</p>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <a
+              href='https://www.imdb.com/video/vi3600860953/?playlistId=tt15398776&ref_=ext_shr_lnk'
+              target='_blank'
+              rel='external noreferrer'
+              className="h-10 w-[150px] flex items-center justify-center font-semibold font-mono uppercase border border-white bg-white/10 rounded-md"
+            >Trailer</a>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
